bf4cd60fe80a0c13b26e673443e4d19673838eb5
Haryel Ramalho <haryel.dev@gmail.com>
2020-11-02 15:18:48 +0000

    Updates

---
+++ precache-manifest.13c6bd9bbed1356570e40c9e226e1d02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56610c236e42daa2018765e3f5997cd",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "2810e929c76e05c701bc",
+    "url": "/github-explorer/static/js/2.15c0aa4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.15c0aa4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b489fa3dbe4d50042af1",
+    "url": "/github-explorer/static/js/main.92583052.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-bg.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
